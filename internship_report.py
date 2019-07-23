e1ca8b3fe571f427e2f1a21cf71921631b8cd7ed
whgjfl <47092856+whgjfl@users.noreply.github.com>
2019-07-23 12:23:38 +0900

    Update internship_report.py

--- internship_report.py
+++ internship_report.py
@@ -1,7 +1,69 @@
-# Implement paper. The selfie
-# Selfie is kind of pretraining. improve accuracy and get stable deviation
-# Selfie is used model as transformer, BERT, ResNet
+#### index ####
+# 1. Subject
+# 2. Reference
+# 3. Task
+# 4. Detail
+# 5. Progress
+# 6. Plan
 
+#### Subject ####
+# My subject is implement the paper, The selfie. That is kind of pretraining. 
+# what is pretraining? 
+# pretraining is to use part of already trained model when train the model
+# what is advantage of Selfie?
+# using selfie is improved accuracy and get stable deviation than when not use to selfie
+
+#### Reference ####
+# Selfie is reference to model that transformer, BERT, ResNet
+
+#### Task ####
+# Selfie is consist of below task
+# 1. Data preprocessing
+# 2. Image processing
+# 3. Masking
+# 4. Attention pooling
+# 5. Positional embedding
+# 6. Prediction
+# 7. Finetuning
+
+#### Deatail ####
+# 1. Data preprocessing
+#  perform zero padding to input data
+#  randomized crop to original size
+#  slicing to tiles(tile number is intentionally decide)
+#
+# 2. Image processing
+#  each sliced tile is process by P. Then feature of tile is extracted
+#  (P is three conv block of ResNet50)
+#
+# 3. Masking
+#
+# 4. Attention pooling
+#  input  : each feature vector of tile(ie. h)
+#  output : single vector(ie. u) 
+#  pooling is use transformer layer
+#  use attention block of BERT 
+#
+# 5. Positional embedding
+#  use to transformer's position embedding 
+#  if input tile of masked out to decoder then  
+#
+# 6. Prediction
+#  add PE of masked out tile with u(ie. v)
+#  dot product v with h'1 ~ h'n
+#  softmax v*h'1 ~ v*h'n then predict correct tile
+#
+# 7. Finetuning
+#  replace three conv block of train model to learned P 
+
+
+#### Progress ####
+# performing task 3 
+
+#### Plan ####
+#
+#
+#
 
 '''
 주제
