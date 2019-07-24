3c64ad430b0cfedde9e391be36e744df9adad930
whgjfl <47092856+whgjfl@users.noreply.github.com>
2019-07-24 22:06:59 +0900

    Update internship_report.py

--- internship_report.py
+++ internship_report.py
@@ -71,6 +71,29 @@
 #### Reference ####
 # Selfie is reference to model that transformer, BERT, ResNet
 # I study reference paper for implement the Selfie 
+#
+# transformer
+# NLP 모델의 한 종류
+# 동작 원리는 유사도 측정을 하여 가장 유사한 단어에 신경 쓰는 것.
+# 인코더와 디코더로 이루어져 있고, 인코더는 2개의 서브레이어로 구성되어 있으며
+# 서브 레이어는 멀티 어텐션 헤드 > 잔차 + 노말라이즈 > 피드포워드 네트워크(레이어 + 함수 + 레이어) > 잔차 + 노말라이즈
+# 디코더
+# 측정하려는 값을 입력데이터로 사용하고 멀헤 > 잔차 + 노말라이즈 > 거쳐 인코더에서 나온 쿼리, 벡터, 벨류와 아웃풋을 펑션을 사용하여 계산 후 
+# 피드포워드 네트워크 > 잔차 + 노말라이즈를 거쳐 일차항으로 변환한 후 소프트맥스를 이용하여 다음 단어를 예측함. 
+# use attention fuction(attention is compute to similarity next word)
+#
+# BERT
+# 트랜스포머의 인코더를 이용한 NLP 모델 
+# 셀프 어텐션을 이용하여 구성되어있으며, 마스크 모델이 있음
+#  
+#
+#
+# ResNet 
+# 잔차 네트워크를 이용한 분류 모델. 
+# 자기지도? 비지도? 뭔지 똑바로 알고 서술할 것
+#
+#
+
 
 #### Task ####
 # Selfie is consist of below task
@@ -84,34 +107,41 @@
 
 #### Deatail ####
 # 1. Data preprocessing
+#    목적과 왜 이렇게 하는지 이유
 #  perform zero padding to input data
 #  randomized crop to original size
 #  slicing to tiles(tile number is intentionally decide)
 #
 # 2. Image processing
+#    목적과 왜 이렇게 하는지 이유
 #  each sliced tile is process by P. Then feature of tile is extracted
 #  (P is three conv block of ResNet50)
 #
 # 3. Masking
+#    목적과 왜 이렇게 하는지 이유
 #  tile is intentinonally masked out(13 tiles masked out is used in Selfie paper)
 #  use masking skill of BERT 
 #
 # 4. Attention pooling
+#    목적과 왜 이렇게 하는지 이유
 #  input  : each feature vector of tile(ie. h)
 #  output : single vector(ie. u) 
 #  pooling is use transformer layer
 #  use attention block of BERT 
 #
 # 5. Positional embedding
+#    목적과 왜 이렇게 하는지 이유
 #  use to transformer's position embedding 
 #  if input tile of masked out to decoder then  
 #
 # 6. Prediction
+#    목적과 왜 이렇게 하는지 이유
 #  add PE of masked out tile with u(ie. v)
 #  dot product v with h'1 ~ h'n
 #  softmax v*h'1 ~ v*h'n then predict correct tile
 #
 # 7. Finetuning
+#    목적과 왜 이렇게 하는지 이유
 #  replace three conv block of train model to learned P 
 
 
